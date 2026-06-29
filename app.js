6e576b6fd1a7d256249d6f8053a62c8185bfe9a9
huan <tanhuan.klord@gmail.com>
2026-06-29 10:46:01 +0700

    init files - v79

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* ===== API config (Cloudflare Pages frontend -> Apps Script JSON API backend) ===== */
-var API_URL = 'https://script.google.com/macros/s/AKfycbwk_Zm5bTDLw0BRhN0qQ0unrCWOcBxhjF9xcyMK83INbcwx4l4bi9YJuY7qh2OJzbfE/exec'; // .../exec
+var API_URL = 'https://script.google.com/macros/s/AKfycbyp90JAJy9t9XpviFU__z6j53fEIvBqKQNuTbc_m4Z0p21IpzKxfEkUhPhx6-vWcqvEXg/exec'; // .../exec
 
 function apiCall(action, payload){
   if(!payload){
